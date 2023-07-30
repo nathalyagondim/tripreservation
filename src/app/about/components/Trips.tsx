e6344cf9bcebfd8387f6a93bb3da9a9f74f9f9af
nathalyagondim <na.gondims2@gmail.com>
2023-07-29 22:09:57 -0300

    feat

---
+++ src/app/about/components/Trips.tsx
@@ -0,0 +1,19 @@
+import { prisma } from "@/lib/prisma";
+import React, { useState } from "react";
+
+const getTrips =async () => {
+    const trips = await prisma.trip.findMany({})
+    return trips
+}
+
+const Trips = async () => {
+    const data = await fetch("http://jsonplaceholder.typecode.com/posts").then((res) => res.json())
+    
+    return <div>
+        {data.map((i: any) => (
+            <p key={i.id}> {i.title}</p>
+        ))}
+    </div>
+}
+
+export default Trips
---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Trips from "./components/trips";
+
+const Page = () => {
+    return (
+        <div>
+            <Trips />
+        </div>
+    )
+}
+
+export default Page
